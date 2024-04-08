c2faea21230e46d573f621bfb6ce8223284e19f0
nsfisis <nsfisis@gmail.com>
2024-04-08 12:42:20 +0900

    medium/00016-medium-pop

--- playground/medium/00016-medium-pop.ts
+++ playground/medium/00016-medium-pop.ts
@@ -26,7 +26,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer Rest, any] ? Rest : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
