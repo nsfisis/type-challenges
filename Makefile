3a4c695b459ccd48e41b6e3f0c82a6ac2b39fef7
nsfisis <nsfisis@gmail.com>
2024-04-09 23:21:58 +0900

    add `make icommit` task

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 .PHONY: count
 count:
-	@git log --oneline origin/main..HEAD | rg '^\w+ \w+/\d+-' | cut -d ' ' -f 2- | tac | bat -n
+	@git log --oneline origin/main..HEAD | rg '^\w+ \w+/\d+-' | cut -d ' ' -f 2- | tac | bat -fn | tail -n 20
 
 .PHONY: commit
 commit:
 	@bash commit.sh
+
+.PHONY: icommit
+icommit:
+	@bash commit.sh ' (incomplete)'
--- commit.sh
+++ commit.sh
@@ -1,9 +1,15 @@
-set -eux
+set -eu
+
+if [[ $# == 1 ]]; then
+    c=$1
+else
+    c=''
+fi
 
 n="$(git status --short | grep '\bplayground/.*[.]ts' | wc -l)"
 if [[ $n == 1 ]]; then
     a="$(git status --short | grep -o '\bplayground/.*[.]ts')"
     git add "$a"
     m="$(git status --short | grep -o '\bplayground/.*[.]ts' | sed -e 's|^playground/||' -e 's|[.]ts$||')"
-    git commit --message "$m"
+    git commit --message "$m$c"
 fi
