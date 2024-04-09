1af1fb807c82249852191c9878651a567896827d
nsfisis <nsfisis@gmail.com>
2024-04-09 23:07:20 +0900

    medium/00110-medium-capitalize

--- playground/medium/00110-medium-capitalize.ts
+++ playground/medium/00110-medium-capitalize.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyCapitalize<S extends string> = any
+type MyCapitalize<S extends string> = S extends `${infer Head}${infer Tail}` ? `${Uppercase<Head>}${Tail}` : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
