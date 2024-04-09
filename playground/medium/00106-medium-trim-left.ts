9c40d37ea18db7fb5e915616059d86eb1e4969a2
nsfisis <nsfisis@gmail.com>
2024-04-09 23:03:57 +0900

    medium/00106-medium-trim-left

--- playground/medium/00106-medium-trim-left.ts
+++ playground/medium/00106-medium-trim-left.ts
@@ -18,7 +18,9 @@
 
 /* _____________ Your Code Here _____________ */
 
-type TrimLeft<S extends string> = any
+type Whitespace = ' ' | '\t' | '\r' | '\n'
+type TrimLeft<S extends string> =
+  S extends `${Whitespace}${infer Rest}` ? TrimLeft<Rest> : S
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
