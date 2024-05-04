68e9a9f31058db42ed74bf5bc8f890217f7838d1
nsfisis <nsfisis@gmail.com>
2024-05-04 22:53:48 +0900

    medium/04425-medium-greater-than (incomplete)

--- playground/medium/04425-medium-greater-than.ts
+++ playground/medium/04425-medium-greater-than.ts
@@ -25,7 +25,17 @@
 
 /* _____________ Your Code Here _____________ */
 
-type GreaterThan<T extends number, U extends number> = any
+type Nat = 0[]
+type NatZero = []
+type NatSucc<N extends Nat> = [0, ...N]
+type NumberToNatHelper<T extends number, N extends Nat> = T extends NatToNumber<N> ? N : NumberToNatHelper<T, NatSucc<N>>
+type NatToNumber<N extends Nat> = N['length']
+type NumberToNat<T extends number> = NumberToNatHelper<T, NatZero>
+
+type NatGe<N extends Nat, M extends Nat> = N extends [...M, ...any[]] ? true : false
+type NatEq<N extends Nat, M extends Nat> = N extends M ? true : false
+type NatGt<N extends Nat, M extends Nat> = NatEq<N, M> extends true ? false : NatGe<N, M>
+type GreaterThan<T extends number, U extends number> = NatGt<NumberToNat<T>, NumberToNat<U>>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
