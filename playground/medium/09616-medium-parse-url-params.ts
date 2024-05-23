09b0c8ada2fb54b5ce055bd0542b931bc1b46b74
nsfisis <nsfisis@gmail.com>
2024-05-24 02:06:59 +0900

    medium/09616-medium-parse-url-params (skipped)

--- playground/medium/09616-medium-parse-url-params.ts
+++ playground/medium/09616-medium-parse-url-params.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type ParseUrlParams<T> = any
+type ParseUrlParams<T> = TODO
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
