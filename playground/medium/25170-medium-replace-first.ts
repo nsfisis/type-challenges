14a6ad27965675317de496b8d6538afc70176fcd
nsfisis <nsfisis@gmail.com>
2024-05-29 19:34:31 +0900

    medium/25170-medium-replace-first

--- playground/medium/25170-medium-replace-first.ts
+++ playground/medium/25170-medium-replace-first.ts
@@ -12,7 +12,10 @@
 
 /* _____________ Your Code Here _____________ */
 
-type ReplaceFirst<T extends readonly unknown[], S, R> = any
+type ReplaceFirst<T extends readonly unknown[], S, R> =
+  T extends [infer Head, ...infer Tail]
+    ? (Head extends S ? [R, ...Tail] : [Head, ...ReplaceFirst<Tail, S, R>])
+    : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
